3c679e447186bcbfefec5bb55e26778b3eb060c3
bektemiroff99-hash <bektemiroff99@gmail.com>
2026-07-07 17:58:06 +0500

    Add: Talaba ma'lumot kartasi [bektemiroff99-hash]

--- lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/06-Stringlar___kirish__matn__qo_shtirnoqlar__len__/01-Talaba_ma_lumot_kartasi.py
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/06-Stringlar___kirish__matn__qo_shtirnoqlar__len__/01-Talaba_ma_lumot_kartasi.py
@@ -1,1 +1,9 @@
-input
+ism = "Aziz Karimov"
+kurs = "Python"
+shahar = "Toshkent"
+
+print("=== KARTA ===")
+print("Ism:", ism)
+print("Kurs:", kurs)
+print("Shahar:", shahar)
+print("Ism uzunligi:", len(ism))
