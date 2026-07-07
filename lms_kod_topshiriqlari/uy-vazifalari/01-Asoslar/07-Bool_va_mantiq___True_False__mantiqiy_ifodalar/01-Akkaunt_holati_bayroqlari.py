5e1d1caaec2508f353f45491345f0fda32fa50e4
bektemiroff99-hash <bektemiroff99@gmail.com>
2026-07-07 18:05:28 +0500

    Add: Akkaunt holati bayroqlari [bektemiroff99-hash]

--- lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/07-Bool_va_mantiq___True_False__mantiqiy_ifodalar/01-Akkaunt_holati_bayroqlari.py
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/07-Bool_va_mantiq___True_False__mantiqiy_ifodalar/01-Akkaunt_holati_bayroqlari.py
@@ -1,1 +1,6 @@
-input
+royxatdan_otgan = True
+email_tasdiqlangan = False
+bloklangan = False
+print("Ro'yxatdan o'tgan:",royxatdan_otgan)
+print("Email tasdiqlangan:",email_tasdiqlangan) 
+print("Bloklangan:",bloklangan)
