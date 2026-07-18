c62d78138c6c6cdd626ade793e954e370a09be42
bektemiroff99-hash <bektemiroff99@gmail.com>
2026-07-18 18:32:53 +0500

    Add: Kabisa yili [bektemiroff99-hash]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/01-if___elif___else_____shart_operatorlari_asoslari/01-Kabisa_yili.py
@@ -0,0 +1,9 @@
+yil = int(input())
+if yil % 400 == 0:
+        print("kabisa")
+elif yil % 100 == 0:
+        print("oddiy")
+elif yil % 4 == 0:
+        print("kabisa")
+else:
+        print("oddiy")
